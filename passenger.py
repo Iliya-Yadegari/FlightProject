6538d1ab737c3b2903814ae604da4d1b97bdce36
Iliya-Yadegari <naruto3@email.com>
2021-08-25 20:40:26 +0100

    Update passenger.py

--- passenger.py
+++ passenger.py
@@ -2,3 +2,6 @@ class Passenger:
     def __init__(self,name,passportNumber):
         self.name = name
         self.passportNumber = passportNumber
+    
+    def __str__(self):
+        return f'self.name'
