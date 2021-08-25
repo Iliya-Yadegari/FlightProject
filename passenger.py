0c6a648f7881981628828fb01244baeab5c408a4
Iliya-Yadegari <naruto3@email.com>
2021-08-25 12:13:56 +0100

    Update passenger.py

--- passenger.py
+++ passenger.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 22 20:33:23 2021
-
-@author: narut
-"""
-
+class Passenger:
+    def __init__(self,name,passportNumber):
+        self.name = name
+        self.passportNumber = passportNumber
