07d19b6173b0285a1fb77329f9bc39673328ab33
Iliya-Yadegari <naruto3@email.com>
2021-08-27 12:49:29 +0100

    Update flight.py

--- flight.py
+++ flight.py
@@ -1,5 +1,5 @@
 class Flight:
-    def __init__(self,flightNumber,origin,destination,distance,passengers):
+    def __init__(self,flightNumber,origin,destination,distance):
 
         self.flightNumber = flightNumber
         self.origin = origin
@@ -8,7 +8,8 @@ class Flight:
         self.passengers = []
         
     def __str__(self):
-        return f'Flight number {self.flightNumber} from YYZ to FRA with distance {self.distance}km.'
+        return f'Flight number {self.flightNumber} from {self.origin.code} to {self.destination.code} with distance {self.distance}km.'
     
-    def addPassenger(self,p):
-        self.passengers.append(p)
+    def addPassenger(self,newP):
+        self.passengers.append(newP)
+        newP.flights.append(self)
