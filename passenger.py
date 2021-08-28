ec875d9c97055fb3c0be5e88e2354e9d2793a0f1
Iliya-Yadegari <naruto3@email.com>
2021-08-28 18:47:03 +0100

    Update passenger.py

--- passenger.py
+++ passenger.py
@@ -3,3 +3,19 @@ class Passenger:
         self.name = name
         self.passportNumber = passportNumber
         self.flights = []
+    
+    def calculatePoints(self):
+        
+        p = 0
+        
+        for f in self.flights:
+            if f.origin.country == f.destination.country:
+                
+                p += 1
+                
+            
+            else:
+                
+                p += 3
+        
+        return p
